60be7f2471a9315dc8e8a59d93ddb54fac5129bb
Hakan Yildiz <hakan@re-coded.com>
2020-04-06 08:44:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,3 +3,16 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(record){
+   
+    
+    record.find(function(e){
+      return e === "W";
+    });
+    
+    function(record.year) {
+     
+   }
+  
+}
